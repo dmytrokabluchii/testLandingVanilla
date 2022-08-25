eb134050a625a3f5d0b7a52664b40b680790bb0b
Dmytro <90714734+dmytrokabluchii@users.noreply.github.com>
2022-08-25 15:09:19 +0300

    update projects,burger-menu,validation,etc

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,13 +1,15 @@
 window.addEventListener('DOMContentLoaded', () => {
   const header = document.getElementById("header_testing"),
   anchors = document.querySelectorAll(".header__logo_symbol, .nav__menu_link"),
-  burgerButton = document.querySelectorAll(".hamburger"),
-  burgerToggle = document.querySelectorAll(".hamburger, .mobile_menu_wrap"),
+  burgerButton = document.querySelector(".hamburger__menu"),
+  mobileMenu = document.querySelector(".mobile__menu"),
   burgerLinks = document.querySelectorAll(".sidemenu ul li a, .mobile__logo_symbol"),
   modalWindow = document.querySelector('.modal__window'),
+  modalField = document.querySelector('.modal__field'),
   overlay = document.querySelector('.overlay'),
   btnModalOpen = document.querySelectorAll('[data-modal]'),
-  btnModalClose = document.querySelector('.modal__close_btn');
+  btnModalClose = document.querySelector('.modal__close_btn'),
+  contactForm = document.querySelectorAll('.contact__form');
 
   // fix header
   function fixHeader() {
@@ -46,30 +48,25 @@ window.addEventListener('DOMContentLoaded', () => {
   smoothScroll();
 
   // Hamburger-menu
-  // $(".hamburger, .page_overlay").on('click', function () {
-  //   $(".mobile_menu_wrap .hamburger").toggleClass("is-active");
-  //   $("body").toggleClass("open");
-  // });
-  // // close burger
-  // $(".sidemenu ul li a, .mobile__logo_symbol").on('click', function () {
-  //     $("body").removeClass("open");
-  // });
-
-  function burgerMenu() {
-      burgerButton.forEach(btn => {
-        btn.addEventListener("click", () => {
-          btn.classList.toggle("is-active");
-          document.body.classList.toggle('open');
-        });
+  function burgerMenu(btn, menu, links) {
+    btn.addEventListener('click', function(){
+      btn.classList.toggle('active');
+      menu.classList.toggle('active');
+      document.body.classList.toggle('open');
+      // добав-м стиль не позвол-й прокручивать стр-цу
+      document.body.classList.toggle('overflow');
+    });
+    // Закрыти бургер-меню при нажатии на ссылку
+    links.forEach(closeBtn => {
+      closeBtn.addEventListener("click", () => {
+        btn.classList.remove('active');
+        menu.classList.remove('active');
+        document.body.classList.remove('open');
+        document.body.classList.remove('overflow');
       });
-      // close burger
-      burgerLinks.forEach(closeBtn => {
-        closeBtn.addEventListener("click", () => {
-          document.body.classList.remove('open');
-        });
-      });
+    });
   }
-  burgerMenu();
+  burgerMenu(burgerButton, mobileMenu, burgerLinks);
 
   // init Swiper:
   function startSwiper() {
@@ -116,8 +113,6 @@ window.addEventListener('DOMContentLoaded', () => {
     // скрываем и показыв-м наше мод. окно
     modalWindow.classList.toggle('active');
     overlay.classList.add('active');
-    // modalWindow.classList.add('show-modal');
-    // modalWindow.classList.remove('hide-modal');
     // когда мы откр-м модал. окно у нас добав-ся стиль не позвол-й прокручивать стр-цу
     document.body.style.overflow = 'hidden';
   }
@@ -128,29 +123,196 @@ window.addEventListener('DOMContentLoaded', () => {
   function closeModal() {
     modalWindow.classList.toggle('active');
     overlay.classList.remove('active');
-    // modalWindow.classList.add('hide-modal');
-    // modalWindow.classList.remove('show-modal');
     // восстанав-м скролл на стр-це после закрытия, если остав-м '' то браузер сам решит что ставить по дефолту
     document.body.style.overflow = '';
   }
-  // только после сраб-я клика у нас выпол-ся фун-я closeModal
-  btnModalClose.addEventListener('click', closeModal);
-  // реал.функ-л закрытия модал. окна по клику вне мод. окна (not working!)
-  modalWindow.addEventListener('click', (e) => {
+  // реал.функ-л закрытия модал. окна по клику вне мод. окна и на "крестик"
+  modalField.addEventListener('click', (e) => {
     // e.target - куда кликнул польз-ль
-    if(e.target === modalWindow) {
+    if(e.target === modalField || e.target.getAttribute('data-close') == '') {
         closeModal();
     }
   });
   // закрытия модал. окна по ESС. Cобытие keydown срабатывает, когда клавиша была нажата
     document.addEventListener('keydown', (e) => {
       // у нашего объекта событие (e) есть св-во .code, кот-е может отслеж-ть код нашей клавиши
-      // Метод Node.contains() возвращает Boolean, проверяет, находится ли элемент ('show') в теле страницы.
+      // Метод Node.contains() возвращает Boolean, проверяет, находится ли элемент ('active') в теле страницы.
       if (e.code === 'Escape' && modalWindow.classList.contains('active')) {
           closeModal();
       }
   });
 
+
+  // send form to my Telegram BOT and Validate
+  const nameInput = document.getElementById('form_name'),
+        emailInput = document.getElementById('form_email'),
+        phoneInput = document.getElementById('form_phone'),
+        typeJobs = document.getElementById('form_type_jobs'),
+        messageInput = document.getElementById('form_message'),
+        BOT_TOKEN = '5324396066:AAFDhE5HZ4_mI54HC4OmzWCfjxawduNh8S8',
+        CHAT_ID = '-1001758890997';
+
+  const message = {
+    loading: 'assets/images/svg/spinner.svg',
+    failureName: 'Write you name',
+    failurePhone: 'Write correct your phone number',
+    failureEmail: 'Write correct your email address'
+  };
+
+  // Цвет-е оформление бордера. Проверка на пустую строку по имени
+  function validationFormColor(elem) {
+    elem.addEventListener('input', () => {
+      if (elem.value !== '') {
+        elem.style.borderColor = 'green';
+      } else {
+        elem.style.borderColor = 'red';
+      }
+    });
+  }
+  validationFormColor(nameInput);
+  validationFormColor(typeJobs);
+  validationFormColor(messageInput);
+  
+  function showSuccess() {
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: 'Your message send, we are connect with you soon!',
+      showConfirmButton: false,
+      timer: 5000,
+    });
+  }
+  function showInfoValidate() {
+    Swal.fire({
+      position: 'top-end',
+      icon: 'info',
+      title: 'Fill all field marked *!',
+      showConfirmButton: false,
+      timer: 4000
+    });
+  }
+  function showError() {
+    Swal.fire({
+      position: 'top-end',
+      icon: 'error',
+      title: 'Server error!',
+      showConfirmButton: false,
+      timer: 3000
+    });
+  }
+  // Phone-number mask
+  function validatePhone() {
+    [].forEach.call(document.querySelectorAll('#form_phone'), function(input) {
+      let keyCode;
+      function mask(event) {
+        event.keyCode && (keyCode = event.keyCode);
+        // Element.selectionStart - повертає/задає позицію початку виділення у текстовому полі textarea або input
+        let pos = this.selectionStart;
+        if (pos < 3) event.preventDefault();
+        let matrix = "+38(___)___-__-__",
+          i = 0,
+          def = matrix.replace(/\D/g, ""),
+          val = this.value.replace(/\D/g, ""),
+          newValue = matrix.replace(/[_\d]/g, function(a) {
+              return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+          });
+        i = newValue.indexOf("_");
+        if (i != -1) {
+          i < 5 && (i = 3);
+          newValue = newValue.slice(0, i);
+        }
+        let reg = matrix.substr(0, this.value.length).replace(/_+/g,
+          function(a) {
+            return "\\d{1," + a.length + "}";
+          }).replace(/[+()]/g, "\\$&");
+        reg = new RegExp("^" + reg + "$");
+        if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+          this.value = newValue;
+        }
+        if (event.type == "blur" && this.value.length < 5) {
+          this.value = "";
+        } 
+
+        // Доработать
+        phoneInput.addEventListener('input', onInputPhone); 
+        function isPhoneValid(value) {
+          return reg.test(value);
+        }
+        function onInputPhone() {
+          if (isPhoneValid(phoneInput.value)) {
+            this.style.borderColor = 'green';
+          } else {
+            this.style.borderColor = 'red';
+          }
+        }
+
+      }
+      input.addEventListener("input", mask, false);
+      input.addEventListener("focus", mask, false);
+      input.addEventListener("blur", mask, false);
+      input.addEventListener("keydown", mask, false);
+    });
+  }
+  validatePhone();
+  
+  // Validate EMAIL
+  function validateEmail(email) {
+    const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+    function onInputEmail() {
+      if (isEmailValid(email.value)) {
+        email.style.borderColor = 'green';
+      } else {
+        email.style.borderColor = 'red';
+      }
+      return;
+    }
+    email.addEventListener('input', onInputEmail); 
+    function isEmailValid(value) {
+      return EMAIL_REGEXP.test(value);
+    }
+    // return EMAIL_REGEXP.test(String(email).toLowerCase());
+  }
+  validateEmail(emailInput);
+
+  // берем все наши формы и под каждую из них подвяз-м фун-ю postData, она и будет обработ-м события при отправке
+  document.querySelectorAll('form').forEach(item => { 
+    postData(item); 
+  });
+  function postData(form) {
+    // навеш-м событие 'submit' и оно будет сраб-ть каждый раз когда форма отправ-ся
+    form.addEventListener('submit', (e) => {
+      // отмен-м станд-е поведение браузера(у нас это перез-ка при отправке данных)
+      e.preventDefault();
+      let text = encodeURI(`Name: ${nameInput.value};\nEmail: ${emailInput.value};\nPhone: ${phoneInput.value};\nJobs: ${typeJobs.value};\nMessage: ${messageInput.value}`);
+      
+      // Подгружаем спиннер Доработать!
+      // const statusMessage = document.createElement('img');
+      // statusMessage.src = message.loading;
+      // statusMessage.style.cssText = `
+      //     display: block;
+      //     margin: 0 auto;
+      // `;
+      // form.insertAdjacentElement('afterend', statusMessage);
+
+      if (nameInput.value !== '' && emailInput.value !== '' && phoneInput.value !== '') { 
+        axios.get(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=` + 
+        text + '&parse_mode=html') 
+        .then( () => {
+          // console.log(data.status);
+          showSuccess();
+        })
+        .catch( () => { 
+          showError(); 
+        })
+        .finally( () => {
+          // очищаем нашу форму после отправки методом reset()
+          form.reset();
+        }); 
+      } else {
+        showInfoValidate();
+      }
+    });
+  }
 
 });
 
@@ -170,3 +332,13 @@ window.addEventListener('DOMContentLoaded', () => {
     document.getElementById(partnersName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+
+
+
+// RegExp.prototype.test()
+// Метод test() выполняет поиск сопоставления регулярного выражения указанной строке. Возвращает true или false.
+
+// selectionStart 
+// властивість об'єкту Element (елементів textarea і input) яка повертає або задає число що представляє позицію 
+// початку виділення. Для отримання кінцевої позиції виділення використовуйте властивість selectionEnd.
